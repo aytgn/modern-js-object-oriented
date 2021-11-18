a05de09f0acb86f93a5128058dcfb8cb9ef2fe4e
Aytugan Özgan <aytgnozgn@gmail.com>
2021-11-18 15:43:30 +0300

    inspecting primitives

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-// Person constructor
-function Person(name, dob) {
-  this.name = name;
-  this.birthday = new Date(dob);
-  this.calculateAge = function () {
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  };
-  this.age = this.calculateAge();
+//string
+const name1 = "Jeff";
+const name2 = new String("Jeff");
+console.log(name2);
+// name2.foo = "bar";
+console.log(name2);
+console.log(typeof name1); //string
+console.log(typeof name2); //object (not primitive anymore)
+if (name2 === "Jeff") {
+  //type is not string
+  console.log("yes");
 }
-
-// console.log(this)//window object; bcs its run in chrome window!
-const brad = new Person("Brad", "9-10-1981");
-const john = new Person("John", 20);
-
-console.log(brad);
-console.log(john);
+if (name2 == "Jeff") {
+  console.log("yesss");
+}
+//number
+const num1 = 5;
+const num2 = new Number(5);
+console.log(typeof num2); //nut primitive value 5 object!
+//function
+const getSum1 = function (x, y) {
+  return x + y;
+};
+console.log(getSum1(1, 1));
+const getSum2 = new Function("x", "y", "return x+y");
+console.log(getSum2(1, 2));
+//object
+const john1 = { name: "John" };
+const john2 = new Object({ name: "Johny" });
+console.log(john1);
+console.log(john2);
+//arrays
+const array1 = [1, 2, 3];
+const array2 = new Array(1, 2, 3);
+console.log(array1, typeof array1);
+console.log(array2, typeof array2);
+//regular expressions
+const re1 = /\w+/;
+const re2 = new RegExp("\\w+");
+console.log(re1);
+console.log(re2);
